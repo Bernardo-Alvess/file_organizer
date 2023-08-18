11dbd602e85ad94c486463d9b9f3e59fa15d31b9
Bernardo Alves <bernardoalves330@gmail.com>
2023-08-18 17:15:48 -0300

    made some changes

--- main.py
+++ main.py
@@ -1,37 +1,59 @@
 import os
+import time
 import shutil
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
-PATH = 'C:/Users/berna/Downloads/'
+def get_download_path():
+    ##Returns the default downloads path for linux or windows
+    if os.name == 'nt':
+        import winreg
+        sub_key = r'SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders'
+        downloads_guid = '{374DE290-123F-4565-9164-39C4925E467B}'
+        with winreg.OpenKey(winreg.HKEY_CURRENT_USER, sub_key) as key:
+            location = winreg.QueryValueEx(key, downloads_guid)[0]
+        return location
+    else:
+        return os.path.join(os.path.expanduser('~'), 'downloads')
+    
+PATH = f'{get_download_path()}\\'
+print(PATH)
+def moveFiles():
+    no_folder_extensions = ['.tmp', '.crdownload', '.opdownload', '.ini']
+    for file in os.listdir(PATH):
+        file_path = f'{PATH}{file}'
+        if os.path.isfile(file_path):
+            extension = os.path.splitext(file)[1]
+            folder_path = os.path.join(PATH, extension)
+            if not os.path.exists(folder_path) and extension not in no_folder_extensions:
+                os.makedirs(folder_path)
+                print(f'Created folder {folder_path}')
+            try:
+                shutil.move(file_path, folder_path)
+            except FileExistsError:
+                #TODO change file name
+                print('nada ainda')
+            except Exception as e:
+                print(e)
+
+moveFiles()
 
 class MyHandler(FileSystemEventHandler):
     def on_any_event(self, event):
         if not event.is_directory:
-            filepath = event.src_path
-            extension = os.path.splitext(filepath)[1]
-            print(extension)
-            if extension != '.tmp':
-                destination_folder = os.path.join(PATH, extension)  # Remove the leading dot from the extension
-                print(destination_folder)
-                if not os.path.exists(destination_folder):
-                    os.makedirs(destination_folder)  # Create the directory if it doesn't exist
-                try:
-                    shutil.move(filepath, destination_folder)
-                    print(f"Moved {filepath} to {destination_folder}")
-                except Exception as e:
-                    print(f"Error moving {filepath}: {e}, tried moving to {destination_folder}")
+            moveFiles()
+while True:
+    if __name__ == "__main__":
+        event_handler = MyHandler()
+        observer = Observer()
+        observer.schedule(event_handler, PATH, recursive=True)
+        observer.start()
 
-if __name__ == "__main__":
-    event_handler = MyHandler()
-    observer = Observer()
-    observer.schedule(event_handler, PATH, recursive=True)
-    observer.start()
+        try:
+            while observer.is_alive():
+                observer.join(1)
+        except KeyboardInterrupt:
+            observer.stop()
 
-    try:
-        while observer.is_alive():
-            observer.join(1)
-    except KeyboardInterrupt:
-        observer.stop()
-
-    observer.join()
+        observer.join()
+    time.sleep(300)
